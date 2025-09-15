c47665b6c3dbc5c477a69ef839901519017e1891
Himansu sekhar Barik <uniquebapun686@gmail.com>
2025-09-15 22:32:15 +0530

    Create Input In Python.py

---
+++ Introduction to python/Input In Python.py
@@ -0,0 +1,18 @@
+#User function Template for python3
+########### Write your code below ###############
+
+# Take string input and print the string input
+
+# Take integer input and add 10 to the integer input and print
+
+# Take floating-point input and multiply the float input by 10 and print
+
+########### Write your code above ###############
+S = input()
+print(S)
+
+n = int(input())
+print(n + 10)
+
+f = float(input())
+print(f * 10)
